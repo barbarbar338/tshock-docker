2ddb10b4ebb288e7037c50373bee2c45b0e22828
Barış DEMİRCİ <hi@338.rocks>
2024-01-07 16:26:24 +0300

    🚑 Fix checker

--- checkCommit.sh
+++ checkCommit.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-exit $(curl -s https://api.github.com/repos/Pryaxis/Tshock/commits/heads/general-devel | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
+repoURL="https://api.github.com/repos/Pryaxis/Tshock/commits/heads/general-devel"
+
+# Get the latest commit date from the repo
+curl -s $repoURL | jq -r ".commit.author.date" | xargs -I{} date -d {} +%s > commitDate.txt
+
+# Get todays date
+today=$(date +%s)
+
+# Get the latest commit date from the file
+commitDate=$(cat commitDate.txt)
+
+echo "Today: $today"
+echo "Last Commit: $commitDate"
+
+# Check if the last commit was more than 24 hours ago
+if [ $((today - commitDate)) -gt 86400 ]; then
+    echo "Last commit was more than 24 hours ago"
+    exit 0
+else
+    echo "Last commit was less than 24 hours ago"
+    exit 1
+fi
